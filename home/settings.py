35a2095c9ad52431e91928c5fa3485f7a2f8a231
Muxriddin <muxriddin@example.com>
2026-05-03 20:20:45 +0500

    'a'a

--- home/settings.py
+++ home/settings.py
@@ -157,8 +157,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Media files (mahsulot rasmlari)
 MEDIA_URL = '/media/'
